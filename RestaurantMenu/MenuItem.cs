358e925220ed8b8bba9b3228e6d551f37cff5436
theresa.lehr17@gmail.com <theresa.lehr17@gmail.com>
2021-06-29 14:49:09 -0500

    committing with all merge conflicts resolved

--- RestaurantMenu/MenuItem.cs
+++ RestaurantMenu/MenuItem.cs
@@ -23,22 +23,7 @@ namespace RestaurantMenu
             this.Price = price;
             this.Description = description;
             this.Category = category;
-            this.TimeDate = DateTime.now;
-
-<<<<<<< HEAD
-        public static void SayIfNew (DateTime RightNow)
-        {
-            DateTime date1 = DateTime.Now;
-            DateTime d2 = new DateTime(date1-1);
-
-            flag = DateTime.Equals(d1, d2);
-
-            if (flag == true)
-                Console.WriteLine("Given dates are equal");
-            else
-                Console.WriteLine("Given dates are not equal");
-        }
-=======
+            this.TimeDate = DateTime.Now;
         }
         public MenuItem(string itemname, double price, string description, string category, string newOrNot)
         {
@@ -49,22 +34,11 @@ namespace RestaurantMenu
             this.New = newOrNot;
             this.TimeDate = DateTime.Now;
             
->>>>>>> 1c6f5a2ae32582b375d3f52f16c3e698a959c57c
 
         }
         public string ItemEntry()
         {
-<<<<<<< HEAD
-            this.itemName = ItemName;
-            this.price = Price;
-            this.description = Description;
-            this.category = Category;
-            Console.WriteLine($"{this.itemName}: ${this.price}\n{this.category}\n{this.description}\nItem Updated at: {rightNow}");
-            
-        }
 
-}
-=======
             Console.WriteLine();
             return $"___________________________________________\n{this.ItemName}: ${this.Price}\n{this.Category}\n{this.Description}\n{this.New}\nAdded on:{TimeDate}\n___________________________________________";
   
@@ -73,11 +47,6 @@ namespace RestaurantMenu
 
 
 
-        // list out all menu items
-        //code last updated
-
-        // this class return list/dictionary? 
-
     }
->>>>>>> 1c6f5a2ae32582b375d3f52f16c3e698a959c57c
+
 }
--- RestaurantMenu/Program.cs
+++ RestaurantMenu/Program.cs
@@ -7,38 +7,11 @@ namespace RestaurantMenu
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            MenuItem cheeseballs = new MenuItem();
-            cheeseballs.ItemEntry("Cheese balls", 5.50, "yummy yummy cheese balls", "lunch");
-=======
-            //Console.WriteLine("Hello James!");
 
 
-            //MenuItem guacamole = new MenuItem("Chips and Guacamole", 10.00, "Homemade Guacamole with corn tortillas", "Appetizer", "New!");
-            //MenuItem breadsticks = new MenuItem("Bread Sticks", 3.00, "Bread sticks with butter and garlic.", "Appetizer", "New!");
-            //MenuItem chips = new MenuItem("Chips and Dip ", 4.99, "Chips with mild salsa.", "Appetizer");
-            //MenuItem wings = new MenuItem("Buffalo Wings", 5.99, "Buffalo Wings with celery and ranch.", "Main Course");
-            //MenuItem steak = new MenuItem("Steak", 15.99, "14oz Ribeye with roasted garlic mashed potatos.", "Main Course");
-            //MenuItem cheeseCake = new MenuItem("Cheese Cake", 5.99,"New York style cheesecake.", "Dessert");
-            //MenuItem applePie = new MenuItem("Apple Pie", 4.99, "Mama's original apple pie ala mode.", "Dessert");
-            //MenuItem fruit = new MenuItem("Fresh Fruit", 4.99,"Assortment of fresh fruits.", "Dessert");
+            Menu menu = new Menu();
+            menu.PrintMenu();
 
-            //guacamole.ItemEntry();
-            //breadsticks.ItemEntry();
-            //chips.ItemEntry();
-            //wings.ItemEntry();
-            //steak.ItemEntry();
-            //cheeseCake.ItemEntry();
-            //applePie.ItemEntry();
-            //fruit.ItemEntry();
-
-            Menu.PrintMenu();
-
-            
-
-            
-
->>>>>>> 1c6f5a2ae32582b375d3f52f16c3e698a959c57c
 
         }
     }
